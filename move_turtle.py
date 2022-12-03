670b0d6d1743150bc63336ea294e81e89eebf335
aditya <adityadhiman912@gmail.com>
2022-12-03 20:18:48 +0000

    Moving turtle in a circle using a python file

---
+++ move_turtle.py
@@ -0,0 +1,24 @@
+import rospy
+from geometry_msgs.msg import Twist
+import sys 
+
+def move_turtle(line_vel, ang_vel):
+    rospy.init_node('turtlemove', anonymous=True)
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(10)
+
+    vel = Twist()
+
+    while True:
+        vel.linear.x = line_vel    
+        vel.linear.y = 2
+        vel.linear.z = 0
+
+        vel.angular.x = 0
+        vel.angular.y = 0
+        vel.angular.z = ang_vel
+
+        pub.publish(vel)
+        rate.sleep()
+
+move_turtle(3.0, 2.5)
